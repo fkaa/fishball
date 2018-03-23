b368bc345b743fd280456b510a83ebb418fa99d1
fkaa <trundmatu@gmail.com>
2018-03-23 22:55:55 +0100

    BUILD: add batch file to fetch dependencies

---
+++ dependencies.bat
@@ -0,0 +1,3 @@
+git clone "https://github.com/glfw/glfw.git" external\glfw
+git clone "https://github.com/cktan/tomlc99.git" external\toml
+git clone "https://github.com/Celtoys/Remotery.git" external\remotery
